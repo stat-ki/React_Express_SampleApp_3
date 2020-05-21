9bc6d379bffd96574be8658c7698e2490f0bcc60
stat-ki <per_ik@outlook.com>
2020-05-21 05:57:24 +0000

    Create root page & routes

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 8080;
+
+app.get('/' , function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection',function(socket){
+    console.log('connected');
+});
+
+http.listen(PORT, function(){
+    console.log('server listening. Port:' + PORT);
+});
